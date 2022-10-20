2ccedce8f7bbf5de5a1dae29387383bf0147f63d
Hritik Singh <hrithiksinghvps_cse18@its.edu.in>
2022-10-20 12:02:45 +0530

    Added cors support?

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 import path from "path"
 import express from 'express';
-const app = express()
-const port = 3000
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-
-const __dirname = path.dirname(__filename);
+import cors from "cors"
 
 import quotes from "./src/quotes.js"
 import characters from "./src/charaters.js"
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+
+const app = express()
+const port = 3000
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const generateRandomindex = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
-app.use(express.static("images"));
+
+
+app.use(cors());
 app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static("images"));
+
+// res.header("Access-Control-Allow-Origin");
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 app.get('/api/quotes', (req, res) => {
   const randomIndex = generateRandomindex(0, quotes.length)
